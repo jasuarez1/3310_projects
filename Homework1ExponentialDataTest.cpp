4fa6b98a564bf9f3e7ff7a89570615f5548fb120
Jonathan Suarez <46019444+jasuarez1@users.noreply.github.com>
2020-02-11 00:18:21 -0800

    Add files via upload

--- Homework1ExponentialDataTest.cpp
+++ Homework1ExponentialDataTest.cpp
@@ -10,18 +10,24 @@ int main()
 {
 	//Initialzatoin area
 	int factorial = 1;
+	long factorial_long=1;
+	double factorial_double=1;
+	float factorial_float=1;
+	short fact=1;
 	int number;
-	//long number;
-	//double number;
-	//float number;
 
 	cout << "Please enter a number" << endl; //Choosing the value for finding the factorial
 	cin >> number;							
-
+	cout<< "The oder for the output will be int ,long,float,double,short"
 	for (int i = 0; i <(number+1); i++)		//Loop that will demonstrate the values of each factorial
 	{
 		factorial += (factorial*i);
-		cout <<i<< "!=" << factorial << endl;
+		factorial_long+=(factorial_long*i);
+		factorial_float+=(factorial_float*i);
+		factorial_double+=(factorial_double*i);
+		fact+=(fact*i);
+
+		cout <<i<< "!=" << factorial << factorial_long << factorial_float << factorial_double << fact <<endl;
 	}
     return 0;
 }
